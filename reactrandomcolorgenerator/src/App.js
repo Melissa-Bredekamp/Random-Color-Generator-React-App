40e10a084159d7aa04010d19a9ed4fa9e95e8d6d
Melissa Bredekamp <melissabredekamp@book-2.public.ph.local>
2020-09-16 14:25:59 +0200

    Second commit

--- reactrandomcolorgenerator/src/App.js
+++ reactrandomcolorgenerator/src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import RandomizedHex from './RandomizedHex';
+// import RequestHueAndLuminosityform from './RequestHueAndLuminosityform';
 
-function App() {
+const App = () => {
+  const [hex, setHex] = useState('#ffffff');
+  // const [AddHue, setHue] = useState('');
+  // const [AddLuminosity, SetLuminosity] = useState('');
+
+  // const AddHue = (hue) => {
+  //   setHue(hue);
+  // };
+
+  // const AddLuminosity = (luminosity) => {
+  //   setLuminosity(luminosity);
+  // };
+  const RandomizedHex = () => {
+    const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    setHex(randomColor);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App"
+      style={{
+        backgroundColor: `${hex}`,
+        minHeight: '33.34vh',
+        overflow: 'hidden',
+        zoom: 4,
+      }}
+    >
+      <h1>{hex}</h1>
+      <button onClick={RandomizedHex}>Randomize</button>
+      {/* <RequestHueAndLuminosityform value={AddHue} value={AddLuminosity} /> */}
     </div>
   );
-}
+};
 
 export default App;
