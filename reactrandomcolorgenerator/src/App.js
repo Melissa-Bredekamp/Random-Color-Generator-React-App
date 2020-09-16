9dd492e55b446a4d7ae6e98a7b0725a75510274a
Melissa Bredekamp <melissabredekamp@Melissas-MacBook-Air.local>
2020-09-16 22:05:17 +0200

    Third commit

--- reactrandomcolorgenerator/src/App.js
+++ reactrandomcolorgenerator/src/App.js
@@ -1,39 +1,58 @@
 import React, { useState } from 'react';
 import './App.css';
-import RandomizedHex from './RandomizedHex';
-// import RequestHueAndLuminosityform from './RequestHueAndLuminosityform';
 
 const App = () => {
   const [hex, setHex] = useState('#ffffff');
-  // const [AddHue, setHue] = useState('');
-  // const [AddLuminosity, SetLuminosity] = useState('');
+  const [hue, setHue] = useState('');
+  const [luminosity, setLuminosity] = useState('');
 
-  // const AddHue = (hue) => {
-  //   setHue(hue);
-  // };
+  const handleSubmit = (event) => {
+    event.preventDefault();
 
-  // const AddLuminosity = (luminosity) => {
-  //   setLuminosity(luminosity);
-  // };
+    setHue(hue);
+  };
+
   const RandomizedHex = () => {
     const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
     setHex(randomColor);
   };
 
   return (
-    <div
-      className="App"
-      style={{
-        backgroundColor: `${hex}`,
-        minHeight: '33.34vh',
-        overflow: 'hidden',
-        zoom: 4,
-      }}
-    >
-      <h1>{hex}</h1>
-      <button onClick={RandomizedHex}>Randomize</button>
-      {/* <RequestHueAndLuminosityform value={AddHue} value={AddLuminosity} /> */}
-    </div>
+    <>
+      <div
+        className="App"
+        style={{
+          backgroundColor: `${hex}`,
+          minHeight: '33.34vh',
+          overflow: 'hidden',
+          zoom: 4,
+        }}
+      >
+        <h1>{hex}</h1>
+        <button onClick={RandomizedHex}>Randomize</button>
+
+        <form onClick={handleSubmit}>
+          <input
+            type="text"
+            value={hue}
+            required
+            onChange={(e) => setHue(e.target.value)}
+          ></input>
+          <button className="color" type="submit" onClick={() => hue}>
+            Add hue
+          </button>
+          <input
+            type="text"
+            value={luminosity}
+            required
+            onChange={(e) => setLuminosity(e.target.value)}
+          ></input>
+          <button className="color" type="submit" onClick={() => luminosity}>
+            Add luminosity
+          </button>
+        </form>
+      </div>
+    </>
   );
 };
 
